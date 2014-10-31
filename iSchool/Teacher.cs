4cb408992be8095b3df1191577c17b68926abc75
GiveRespect <chhetriasispkr@gmail.com>
2014-10-31 13:04:29 +0100

    New without salary

---
+++ iSchool/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSchool
+{
+   public class Teacher
+    {
+       private string _name;
+
+       public string Name
+       {
+           get { return _name; }
+           set
+           {
+               CheckName(value); _name = value; }
+       }
+
+       private static void CheckName(string name)
+       {
+           if (String.IsNullOrWhiteSpace(name) || name.Length<3)
+           {
+               throw new ArgumentException("Value is Nullllllloooooooshittttt or less than 2");
+           }
+       }
+
+       public int Salary { get; set; }
+
+        public Teacher(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Salary: {1}", Name, Salary);
+        }
+    }
+}
---
+++ ischooltest/TeacherTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using iSchool;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ischooltest
+{
+    [TestClass]
+    public class TeacherTest
+    {
+
+        private Teacher _teacher;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _teacher = new Teacher("s", 50000);
+        }
+
+        [TestMethod]
+        public void TestName()
+        {
+           
+            Assert.AreEqual("m",_teacher.Name);
+            try
+            {
+                _teacher.Name =null;
+                Assert.Fail();
+            }
+            catch (ArgumentException asd)
+            {
+                Assert.AreEqual("Value is Nullllllloooooooshittttt or less than 2", asd.Message);
+                
+            }
+        }
+
+       
+
+
+    }
+}
+
+
